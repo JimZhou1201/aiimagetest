8ea9e6500a323193f2ff3b4bb0020573569a3049
JimZhou1201 <jimzyh01@gmail.com>
2024-11-18 18:41:11 +0800

    Remove unused SparklesText component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useState } from "react";
 import Image from "next/image";
-import SparklesText from "@/components/ui/sparkles-text";
 import AnimatedGradientText from "@/components/ui/animated-gradient-text";
 
 // 图片尺寸选项
